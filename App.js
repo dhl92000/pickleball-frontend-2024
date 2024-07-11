5326cd46c47c0bd41c732a9d3a858ba9fe283dda
dahee-lee <dahlee97@gmail.com>
2024-07-11 13:51:07 -0400

    files and component set up

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import WindScreen from './screens/WindScreen';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <WindScreen/>
     </View>
+
   );
 }
 
---
+++ screens/WindScreen.js
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+const WindScreen = () => {
+    return <View>
+        <Text>Wind screen!</Text>
+    </View>
+}
+
+export default WindScreen
